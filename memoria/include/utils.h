0427ba84503b74ccb05d0e5c826e66eeab25e1d8
Martina Soteras <msoteras@frba.utn.edu.ar>
2023-04-08 12:52:51 -0700

    a ver si esto me deja commitear

--- memoria/include/utils.h
+++ memoria/include/utils.h
@@ -11,23 +11,6 @@
 #include<string.h>
 #include<assert.h>
 
-#define IP "127.0.0.1"
-#define PUERTO "4444"
 
-typedef enum
-{
-	MENSAJE,
-	PAQUETE
-}op_code;
-
-extern t_log* logger;
-
-void* recibir_buffer(int*, int);
-
-int iniciar_servidor(void);
-int esperar_cliente(int);
-t_list* recibir_paquete(int);
-void recibir_mensaje(int);
-int recibir_operacion(int);
 
 #endif /* UTILS_H_ */
