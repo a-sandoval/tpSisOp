e7f97b5091e779a0b6f53b149fa0536360aa4152
LFLarocca <llarocca@frba.utn.edu.ar>
2023-04-09 17:45:31 +0000

    primero cambios

--- kernel/include/kernel.h
+++ kernel/include/kernel.h
@@ -1,1 +1,21 @@
-//prueba
+/* - Interprete entre el hardware y el software - */
+
+#ifndef KERNEL_H_
+#define KERNEL_H_
+
+#include<stdio.h>
+#include<stdlib.h>
+#include <string.h>
+#include<commons/log.h>
+#include<commons/string.h>
+#include<commons/config.h>
+#include<readline/readline.h>
+
+#include "shared/include/utils.h"
+
+//Por ser servidor
+void iterator(char* value);
+
+//Por ser cliente
+
+#endif /* CLIENT_H_ */
--- kernel/src/kernel.c
+++ kernel/src/kernel.c
@@ -1,1 +1,8 @@
-//prueba
+/* - Interprete entre el hardware y el software - */
+#include "kernel/include/kernel.h"
+
+// CONSOLA - cliente | KERNELL - servidor
+// KERNEL - cliente | MEMORIA - servidor
+
+// KERNEL - cliente | CPU - servidor
+// KERNEL - cliente | FileSystem - servidor
