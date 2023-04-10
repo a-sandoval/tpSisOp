2ed34aa3ea990442e1268bc728ac3c841abd2135
enferd <fandrade@frba.utn.edu.ar>
2023-04-09 23:01:18 -0300

    Se agrega conexion temporal a la consola

--- consola/src/consola.c
+++ consola/src/consola.c
@@ -6,6 +6,7 @@ int main(void) {
     t_log* logger = log_create("consola.log", "consola", 0, LOG_LEVEL_INFO);
     if (logger == NULL) {
         printf("Error al generar logger consola.log \n");
+        return 1;
     }
 
     t_config* config = config_create("consola.config");
@@ -17,11 +18,19 @@ int main(void) {
 
     ip = config_get_string_value(config, "IP_KERNEL");
     puerto = config_get_string_value(config, "PUERTO_KERNEL");
-    
+    conexion = crear_conexion(ip, puerto);
+    if (conexion == -1) { 
+        char *mensaje_error = string_from_format("No se pudo conectar a la Kernel por la ip %s y puerto %s", ip, puerto);
+        log_error(logger, mensaje_error);
+        log_destroy(logger);
+        config_destroy(config);
+        return 1;
+    }
+
     char *comando = readline("Usuario@TUKI $ ");
     log_info(logger, comando);
     string_trim(&comando);
-    while (strcmp(comando, "exit")) {
+    while (strcmp(comando, "EXIT")) {
         free(comando);
         comando = readline("Usuario@TUKI $ ");
         log_info(logger, comando);
