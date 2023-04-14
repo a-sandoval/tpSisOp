d890b758b012cecf7da9f9b198c31b4200ff41f7
a-sandoval <asandoval@frba.utn.edu.ar>
2023-04-14 13:08:30 +0000

    no funciona

--- file-system/src/fileSystem.c
+++ file-system/src/fileSystem.c
@@ -5,9 +5,7 @@ int main(void) {
 
     logger = iniciar_logger("fileSys.log","file-system");
 
-    log_info(logger, "Hola"); 
     t_config* config_fs = iniciarConfiguracion("/home/utnso/tp-2023-1c-toTheEnd/file-system/file-system.config", logger); 
-
 
     escucharAlKernel(); 
 
