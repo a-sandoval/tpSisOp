7c36485c578cfa751cbae4a7e465e29f56776608
a-sandoval <asandoval@frba.utn.edu.ar>
2023-04-26 12:59:45 +0000

    ciclo de instruccion avance flor

--- cpu/src/cicloDeInstruccion.c
+++ cpu/src/cicloDeInstruccion.c
@@ -7,18 +7,13 @@ void cicloDeInstruccion(){
 
     executeCPU();
 }
+
+
  
 void executeCPU(){
         //SET (Registro, Valor) --> Asigna al registro el valor pasado como parámetro.
-        set(t_reg registro, int valor){
-            int tiempoEspera = obtenerTiempoEspera();
-            sleep(tiempoEspera);
-            registro.registro1 = valor;
-        }
+        
 
-        obtenerTiempoEspera(){
-            return confGet("RETARDO_INSTRUCCION");
-        }
         //YIELD --> Desaloja voluntariamente el proceso de la CPU. Devuelve el Contexto de Ejecución actualizado al Kernel.
         
 
@@ -26,3 +21,14 @@ void executeCPU(){
         //EXIT --> Representa la syscall de finalización del proceso. Devuelve el Contexto de Ejecución actualizado al Kernel para su finalización.
     }
 // IR ACTUALIZANDO EL PCB MIENTRAS SE VA EJECUTANDO
+void set(t_reg registro, int valor){
+            int tiempoEspera = obtenerTiempoEspera();
+            sleep(tiempoEspera);
+            registro.registro1 = valor;
+        }
+
+
+
+int obtenerTiempoEspera(){
+            return config_get_int_value(config,"RETARDO_INSTRUCCION"); 
+}
